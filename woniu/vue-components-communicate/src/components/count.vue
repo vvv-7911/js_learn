20f2bb21c317da2c5112ae55749ba8af6b877bac
vvv-7911 <892518104@qq.com>
2019-09-20 21:39:22 +0800

    learn

--- woniu/vue-components-communicate/src/components/count.vue
+++ woniu/vue-components-communicate/src/components/count.vue
@@ -2,20 +2,48 @@
   <div class="count">
     {{msg}}
     <hr/>
-    <h3>{{$store.state.count}}</h3>
-    <button @click="$store.commit('add')">+</button>
+    <!-- <h3>{{$store.state.count}}</h3> -->
+    <h3>{{count}}</h3>
+    <!-- <button @click="$store.commit('add', 10)">+</button> -->
+    <button @click="add(10)">+</button>
     <button @click="$store.commit('reduce')">-</button>
+    <hr/>
+    <p>{{date}}</p>
+    <hr>
+    <p>{{money}}</p>
+    <button @click="makeMoneyAction">挣钱</button>
   </div>
 
 </template>
 
 <script>
 import store from '@/store/index.js'
+import { mapState, mapMutations, mapGetters, mapActions } from 'vuex'
 export default {
   data() {
     return {
       msg: 'Hello Vuex'
     }
+  },
+  // computed: {
+  //   date () {
+  //     return this.$store.state.date
+  //   }
+  // },
+  computed: {
+    ...mapState(['date','money']),
+    ...mapGetters(['count'])
+    // count () {
+    //   return this.$store.getters.count
+    // }
+
+  },
+  // computed: mapState({
+  //   date: state => state.date
+  // }),
+  methods: {
+    ...mapMutations(['add']),
+    ...mapActions(['makeMoneyAction'])
   },
   store
 }
--- woniu/vue-components-communicate/src/store/index.js
+++ woniu/vue-components-communicate/src/store/index.js
@@ -3,20 +3,46 @@ import Vue from 'vue'
 
 Vue.use(Vuex)
 
+// 数据源
 const state = {
-  count: 1
+  count: 1,
+  date: '2019.09.20',
+  money: 100
 }
 
+// 方法  同步
 const mutations = {
-  add (state) {
-    state.count++
+  add (state, n) {
+    state.count += n
   },
   reduce (state) {
     state.count--
+  },
+  makeMoney (state) {
+    state.money += 1
+  }
+}
+
+// 方法  异步
+const actions = {
+  makeMoneyAction ({commit}) {
+    setTimeout(() => {
+      commit('makeMoney')
+    },2000)
+    console.log('挣嘛钱呢！')
+  }
+}
+
+// 计算
+const getters = {
+  count: function(state) {
+    return state.count += 100
   }
 }
 
 export default new Vuex.Store({
   state,
-  mutations
+  mutations,
+  getters,
+  actions
 })
