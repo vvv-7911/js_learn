9f792baa8354c6536dcc88028a270617653c584b
vvv-7911 <892518104@qq.com>
2019-09-23 08:50:16 +0800

    learn

---
+++ FE-videos/VUE/vue-fireim/src/components/MenuList.vue
@@ -0,0 +1,32 @@
+<template>
+    <!-- 容器组件 -->
+    <Menu>
+      <template v-for="menu in data">
+        <MenuItem :key="menu.id" v-if="!menu.children">{{menu.title}}</MenuItem>
+        <ReSubMenu v-else :key="menu.id" :data="menu"></ReSubMenu>
+      </template>
+    </Menu>
+</template>
+
+<script>
+import Menu from './Menu'
+import MenuItem from './MenuItem'
+import ReSubMenu from './ReSubMenu'
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: () => ([])
+    }
+  },
+  components: {
+    Menu,
+    MenuItem,
+    ReSubMenu
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ FE-videos/VUE/vue-fireim/src/components/ReSubMenu.vue
@@ -0,0 +1,25 @@
+<template>
+ <!-- 容器组件 -->
+ <SubMenu>
+   <template #title>{{data.title}}</template>
+   <MenuItem v-for="child in data.children" v-if="!child.children" :key="child.id">{{child.title}}</MenuItem>
+   <ReSubMenu v-else :data="child"></ReSubMenu>
+ </SubMenu>
+</template>
+
+<script>
+import SubMenu from './SubMenu'
+import MenuItem from './MenuItem'
+export default {
+  name: "ReSubMenu",
+  props: ["data"],
+  components: {
+    SubMenu,
+    MenuItem
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
