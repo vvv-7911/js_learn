db0668c596a4886705bd23bc0b5bf4248d761a48
vvv-7911 <892518104@qq.com>
2019-11-08 23:50:55 +0800

    mpvue

---
+++ woniu/mpvue/meituan/src/pages/orderList/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <order-list>
+    韩雨汐辣鸡
+  </order-list>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ woniu/mpvue/meituan/src/pages/self/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="self">
+    韩雨汐辣鸡韩雨汐辣鸡
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
